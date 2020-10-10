94dab723847e3640d55eada944ba28404bcbdb26
predator-alt <72620876+predator-alt@users.noreply.github.com>
2020-10-10 13:13:47 +0600

    Add files via upload

---
+++ zigzag.py
@@ -0,0 +1,23 @@
+import time,sys
+indent = 0 #how many spaces to indent
+indent_increasing = True #whether the indentation is increasing or not.
+try:
+	while True:
+		print(' '*indent,end=' ')
+		print("*******")
+		time.sleep(0.1) #pause for 1/10 of a  second.
+		if indent_increasing:
+			indent= indent +1
+			if indent == 20:
+				indent_increasing = False
+		else:
+			indent= indent-1
+			
+		if indent == 0:
+			indent_increasing = True
+			
+except Keyboardinterrupt:
+			sys.exit()
+			
+			
+		
